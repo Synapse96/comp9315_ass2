8395bcd78aa2061f34adbcaac84610567d3f22b4
tonytran95 <tony.tran@outlook.com.au>
2018-10-20 18:51:05 +1100

    implemented tsigs

--- tsig.c
+++ tsig.c
@@ -10,13 +10,24 @@
 #include "hash.h"
 #include "bits.h"
 
+Bits codeword(char *attr_value, int m, int k);
+
 // make a tuple signature
 
 Bits makeTupleSig(Reln r, Tuple t)
 {
 	assert(r != NULL && t != NULL);
-	//TODO
-	return NULL;
+	Bits tsig = newBits(tsigBits(r));
+	char **tupVals = tupleVals(r, t);
+	for (int i = 0; i < nAttrs(r); i++) {
+		char *tupVal = tupVals[i];
+		if (strcmp(tupVal, "?") != 0) {
+			Bits cw = codeword(tupVal, tsigBits(r), codeBits(r));
+			orBits(tsig, cw);
+		}
+	}
+	//printf("tsig = "); showBits(tsig); printf("\n");
+	return tsig;
 }
 
 // find "matching" pages using tuple signatures
@@ -24,11 +35,41 @@ Bits makeTupleSig(Reln r, Tuple t)
 void findPagesUsingTupSigs(Query q)
 {
 	assert(q != NULL);
-	//TODO
-
-	setAllBits(q->pages); // remove this
-
-	// The printf below is primarily for debugging
-	// Remove it before submitting this function
+	Reln r = q->rel;
+	Bits qsig = makeTupleSig(r, q->qstring);
+	unsetAllBits(q->pages);
+	File tsigs = tsigFile(r);
+	//Bits matches = newBits(nPages(r));
+	for (int i = 0; i < nTsigPages(r); i++) {
+		Page p = getPage(tsigs, i);
+		for (int j = 0; j < pageNitems(p); j++) {
+			Bits tsig = newBits(tsigBits(r));
+			getBits(p, j, tsig);
+			//printf("qsig = "); showBits(qsig); printf("\n");
+			//printf("tsig = "); showBits(tsig); printf("\n");
+			if (isSubset(qsig, tsig)) {
+				setBit(q->pages, i);
+				//printf("they match!\n");
+			}
+			q->nsigs++;
+		}
+		q->nsigpages++;
+	}
+	
 	printf("Matched Pages:"); showBits(q->pages); putchar('\n');
+}
+
+Bits codeword(char *attr_value, int m, int k)
+{
+	int nbits = 0; // count of set bits
+	Bits cword = newBits(m); // assuming m <= 32 bits
+	srandom(hash_any(attr_value, k));
+	while (nbits < k) {
+		int i = random() % m;
+		if (!(bitIsSet(cword, i))) {
+	    	setBit(cword, i);
+	    	nbits++;
+	    }
+	}
+	return cword; // m-bits with k 1-bits and m-k 0-bits
 }
